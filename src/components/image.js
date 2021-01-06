e2a89df53178e8d85fac5b83df9cd5015b638fec
Pradeep K. Pant <pantpradeep@gmail.com>
2021-01-06 12:07:19 +0530

    Update image.js

--- src/components/image.js
+++ src/components/image.js
@@ -18,7 +18,7 @@ const Image = () => {
     query {
       placeholderImage: file(relativePath: { eq: "nanda_devI_sunrise.jpg" }) {
         childImageSharp {
-          fluid(maxWidth: 300) {
+          fluid(maxWidth: 800) {
             ...GatsbyImageSharpFluid
           }
         }
